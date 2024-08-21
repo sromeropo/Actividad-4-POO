169bb393f5658081e84288f54360fb949f67815d
sromeropo <160175381+sromeropo@users.noreply.github.com>
2024-08-20 22:52:34 -0500

    Ejercicio 4.7 Clases Abstractas

---
+++ PruebaAnimales/src/main/java/poo/pruebaanimales/Leon.java
@@ -0,0 +1,21 @@
+
+package poo.pruebaanimales;
+
+public class Leon extends Felino{
+    public String getSonido() {
+        return "Rugido";
+    }
+
+    public String getAlimentos() {
+        return "Carnívoro";
+    }
+  
+    public String getHabitat() {
+        return "Praderas";
+    }
+  
+    public String getNombreCientifico() {
+        return "Panthera leo";
+    }
+    
+}
